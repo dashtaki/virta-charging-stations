c34ef1b831241b8f01a04de21eef95a91945e91f
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-07 23:47:55 +0200

    Fix file name in import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import StationsList from './components/station-list/stations-list';
 import {Route, Switch} from 'react-router-dom'
-import Station from './components/station/satation';
+import Station from './components/station/station';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 //TODO: remove extra file
