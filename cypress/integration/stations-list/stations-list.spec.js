0dd31ce5e6e34a93cc5b08438401b83764f4d917
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-15 00:01:09 +0200

    More test coverage for stations-list.js

--- cypress/integration/stations-list/stations-list.spec.js
+++ cypress/integration/stations-list/stations-list.spec.js
@@ -12,45 +12,40 @@ describe('spinner', () => {
         cy.get('ul').get('li').should('have.length', 27);
     });
 
+    it('should station name', () => {
+        cy.get('ul li:first-child strong').should('contain', 'Endesa');
+    });
+
     it('should put offline icon for offline station', () => {
-        cy.get('ul').within(_ => {
-            cy.get('li').within(_ => {
-                cy.get('span:nth-child(2)').within(_ => {
-                    cy.get('img')
-                        .should('have.attr', 'src', '../offline_icon.png');
-                })
-            })
-        })
+        cy.get('ul li span:nth-child(2) img')
+            .should('have.attr', 'src', '../offline_icon.png');
+    });
+
+    it('should show offline text for offline station', () => {
+        cy.get('ul li span:nth-child(2) p')
+            .should('contain', 'Offline');
     });
 
     it('should put available icon for available station', () => {
-        cy.get('ul').within(_ => {
-            cy.get('li:nth-child(4)').within(_ => {
-                cy.get('span:nth-child(2)').within(_ => {
-                    cy.get('img')
-                        .should('have.attr', 'src', '../available_icon.png');
-                })
-            })
-        })
+        cy.get('ul li:nth-child(4) span:nth-child(2) img')
+            .should('have.attr', 'src', '../available_icon.png');
+    });
+
+    it('should show available text for available station', () => {
+        cy.get('ul li:nth-child(4) span:nth-child(2) p')
+            .should('contain', 'Available');
     });
 
     it('should set "station-availability__text" for text availability', () => {
-        cy.get('ul').within(_ => {
-            cy.get('li').within(_ => {
-                cy.get('span:nth-child(2)').within(_ => {
-                    cy.get('p')
-                        .should('have.class', 'station-availability__text');
-                })
-            })
-        })
+        cy.get('ul li span:nth-child(2) p')
+            .should('have.class', 'station-availability__text');
     });
 
     it('should go to station detail on click', () => {
-        cy.get('ul').within(_ => {
-            cy.get('li:first-child').click();
-            cy.location().should((location) => {
-                expect(location.pathname).to.eq('/station/101')
-            });
+        cy.get('ul li:first-child').click()
+
+        cy.location().should((location) => {
+            expect(location.pathname).to.eq('/station/101')
         });
     });
 })
