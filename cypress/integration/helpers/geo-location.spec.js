967fe18d606cd0ceb8a672922c38f8a66eab8e81
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-14 13:36:15 +0200

    Write unit tests for geo-location.js

---
+++ cypress/integration/helpers/geo-location.spec.js
@@ -0,0 +1,17 @@
+const {retrieveGeoLocation} = require("../../../src/helpers/geo-location");
+describe('geo-location.js', () => {
+    it('should return null if position is null or undefined', () => {
+        const actual = retrieveGeoLocation();
+
+        expect(actual).to.be.null
+    });
+
+
+    it('should return latitude and longitude of position', () => {
+        const expected = {lng: 60.467149, lat: 22.027585};
+        const position = '60.467149,22.027585';
+        const actual = retrieveGeoLocation(position);
+
+        expect(actual).to.deep.eq(expected)
+    });
+})
