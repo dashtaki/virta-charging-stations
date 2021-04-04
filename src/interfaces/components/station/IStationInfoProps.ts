29561b34e24736646afa6e2e22ca4bb29b5af381
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-04-04 14:25:15 +0300

    Convert StationInfo.js to .tsx

---
+++ src/interfaces/components/station/IStationInfoProps.ts
@@ -0,0 +1,7 @@
+import { IStyledComponent } from "../../IStyledComponent"
+import { IStationDetail } from "../../IStationDetail"
+
+export interface IStationInfoProps extends IStyledComponent {
+  station: IStationDetail
+  lastConnect: string
+}
